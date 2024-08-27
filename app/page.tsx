a648d195c2f8f83b422dbff1199f4a551e59d924
Ayroid <ayushsk0000@gmail.com>
2024-08-28 02:13:13 +0530

    fix: add dummy button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { ThemeSwitch } from "@/components/ui/theme-switch";
+import axios from "axios";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, Suspense } from "react";
+import { toast } from "sonner";
 
 function HomeContent() {
   const searchParams = useSearchParams();
@@ -19,6 +21,17 @@ function HomeContent() {
     }
   }, [login, router]);
 
+  const testRouteCall = async () => {
+    try {
+      const response = await axios.get("https://api.ideos.live/api/test");
+      console.log("Columns:", response.data);
+      toast.success("Test Route Works");
+    } catch (error) {
+      console.error("Error fetching columns:", error);
+      toast.error("Error fetching columns");
+    }
+  };
+
   return (
     <main className="flex h-full w-full flex-col items-center justify-center gap-6">
       <div className="flex items-center justify-center gap-6">
@@ -31,6 +44,15 @@ function HomeContent() {
         <Link href="/pages/protected">
           <Button>Protected</Button>
         </Link>
+
+        <Button
+          onClick={() => {
+            testRouteCall();
+          }}
+        >
+          Not Protected
+        </Button>
+
         <ThemeSwitch />
       </div>
     </main>
