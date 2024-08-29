e583a7127aa7ef000f4a39d8205b221b5fdbf7e4
Ayroid <ayushsk0000@gmail.com>
2024-08-29 06:24:13 +0530

    feat: update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/ideos.png" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
