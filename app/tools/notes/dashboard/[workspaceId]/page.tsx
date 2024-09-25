8ef13ebd9377bd6f2a8c55f39432d2088aa18851
rohin079 <rohinmehrotra@gmail.com>
2024-09-25 16:09:53 +0530

    fix: removed QuillEditor props from dashboard page

--- app/tools/notes/dashboard/[workspaceId]/page.tsx
+++ app/tools/notes/dashboard/[workspaceId]/page.tsx
@@ -1,20 +1,12 @@
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
-import QuillEditor from '@/components/notetaking/quill-editor/quill-editor';
-import React from 'react';
+import QuillEditor from "@/components/notetaking/quill-editor/quill-editor";
+import React from "react";
 
 const Workspace = ({ params }: { params: { workspaceId: string } }) => {
-  // You can replace these with actual folderId and fileId as needed
-  const folderId = 'static-folder-id';  // Placeholder value for folderId
-  const fileId = 'static-file-id';      // Placeholder value for fileId
-
   return (
     <div className="relative">
-      <QuillEditor
-        workspaceId={params.workspaceId}
-        folderId={folderId}
-        fileId={fileId}
-      />
+      <QuillEditor />
     </div>
   );
 };
